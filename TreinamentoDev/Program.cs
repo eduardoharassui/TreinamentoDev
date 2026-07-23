2ac81a97e7e071006e7c434588a2c4b3b81aef6d
Eduardo Harassui <harassuieduardo@gmail.com>
2026-07-22 22:14:20 -0300

    Atualização Treinamento Dev

--- TreinamentoDev/Program.cs
+++ TreinamentoDev/Program.cs
@@ -6,25 +6,21 @@ using Servico;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllers();
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Registra os serviços e repositórios
 builder.Services.AddScoped<IVendaServico, VendaServico>();
 builder.Services.AddScoped<IVendaRepositorio, VendaRepositorio>();
 builder.Services.AddScoped<IProdutoServico, ProdutoServico>();
 builder.Services.AddScoped<IProdutoRepositorio, ProdutoRepositorio>();
 
-// Registra o DbContext (necessário para o VendaRepositorio/ProdutoRepositorio funcionarem)
 builder.Services.AddDbContext<AppDBContext>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
